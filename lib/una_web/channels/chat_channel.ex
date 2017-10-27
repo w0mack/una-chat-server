3e8ece7279c1842cd10c55086703ef6a38d7b687
Jay Hayes <ur@iamvery.com>
2017-10-27 14:00:28 -0700

    Implement most basic channel for chatting

---
+++ lib/una_web/channels/chat_channel.ex
@@ -0,0 +1,20 @@
+defmodule UnaWeb.ChatChannel do
+  use UnaWeb, :channel
+
+  def join("chat:" <> _topic, _payload, socket) do
+    {:ok, socket}
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (chat:lobby).
+  def handle_in("shout", payload, socket) do
+    broadcast socket, "shout", payload
+    {:noreply, socket}
+  end
+end
--- lib/una_web/channels/user_socket.ex
+++ lib/una_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule UnaWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", UnaWeb.RoomChannel
+  channel "chat:*", UnaWeb.ChatChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
