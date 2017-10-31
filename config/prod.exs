e5a10fcdf638b6cff7430ae22ed8947316733fec
Jay Hayes <ur@iamvery.com>
2017-10-31 16:43:09 -0500

    Disable origin check in prod
    This is probably a Bad Idea™, but this app is a toy.

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,7 @@ use Mix.Config
 config :una, UnaWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "dreadful-spider-42903.herokuapp.com", port: 443],
+  check_origin: false,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
