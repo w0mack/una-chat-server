8de06907d608f680969078441c85126a2d856e08
Jay Hayes <ur@iamvery.com>
2017-10-31 16:36:42 -0500

    Configure app for heroku

--- lib/una_web/channels/user_socket.ex
+++ lib/una_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule UnaWeb.UserSocket do
   channel "chat:*", UnaWeb.ChatChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
