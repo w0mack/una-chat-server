4ca1071ec3efb446ef688d4f69eaad7257968baf
w0mack <31969184+w0mack@users.noreply.github.com>
2017-11-10 09:49:37 -0600

    Update topic_controller.ex

--- lib/una_web/controllers/topic_controller.ex
+++ lib/una_web/controllers/topic_controller.ex
@@ -1,7 +1,7 @@
 defmodule UnaWeb.TopicController do
   use UnaWeb, :controller
 
-  @topics ["fashion", "walking while texting", "various smells"]
+  @topics ["fashion", "walking while texting", "various smells", "UNA is amazing"]
 
   def index(conn, _params) do
     render(conn, "index.json", topics: @topics)
